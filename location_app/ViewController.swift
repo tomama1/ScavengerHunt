82ea80318461fc9c649e6c7ab44576cf780ca592
Vlad <vpgrigoryan@gmail.com>
2017-11-08 14:35:54 -0800

    added timer

--- location_app/ViewController.swift
+++ location_app/ViewController.swift
@@ -8,8 +8,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var Label: UILabel!
     
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var seconds=30
+    var timer=Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
+        timerLabel.isHidden=true
         locationManager = CLLocationManager()
         
         if let manager = locationManager {
@@ -43,6 +48,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBAction func start(_ sender: UIButton) {
         Label.text="Go to the Kitchen, you have 5 seconds"
+        timerLabel.isHidden=false
+        timer=Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+        sender.isHidden=true
+        
+    }
+    
+    func updateTimer(){
+        seconds -= 1
+        timerLabel.text="\(seconds)"
+        if seconds<=0{
+            timer.invalidate()
+        }
         
     }
     
